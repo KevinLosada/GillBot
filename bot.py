fb0c4be6d98c8ea474a946a067b46edc2b399d84
Kevin Losada <kevin.losada21@gmail.com>
2021-02-03 23:11:41 -0500

    committing to github

--- bot.py
+++ bot.py
@@ -9,19 +9,19 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
-@bot.event
-async def on_message(self):
-    Test =  discord.utils.get(message.guild.members, id = 257372338868191233)
-    Kevin = discord.utils.get(message.guild.members, id = 182650505699393536)
+# @bot.event
+# async def on_message(self):
+#     Test =  discord.utils.get(message.guild.members, id = 257372338868191233)
+#     Kevin = discord.utils.get(message.guild.members, id = 182650505699393536)
 
-    bot_channel = discord.utils.get(message.guild.channels, id=805906867104514050)
-    test_channel = discord.utils.get(message.guild.channels, id=806670890170974208)
+#     bot_channel = discord.utils.get(message.guild.channels, id=805906867104514050)
+#     test_channel = discord.utils.get(message.guild.channels, id=806670890170974208)
 
-    if message.author == self.user:
-        return
+#     if message.author == self.user:
+#         return
     
-    if message.user == Kevin and message.channel == test_channel:
-        await bot.send_message(bot_channel, "Fuck you too bitch " + Kevin.mention)
+#     if message.user == Kevin and message.channel == test_channel:
+#         await bot.send_message(bot_channel, "Fuck you too bitch " + Kevin.mention)
 
 @bot.command()
 async def alive(ctx):
