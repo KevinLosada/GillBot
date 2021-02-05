04a5a21368ccd690f82f4d5b21a9f8a82084ae29
Kevin Losada <kevin.losada21@gmail.com>
2021-02-04 19:12:01 -0500

    Fixed cogs issues

--- cogs/leagueCommands.py
+++ cogs/leagueCommands.py
@@ -1,22 +1,27 @@
-# import os
-# import discord
-# from discord.ext import commands
-# from dotenv import load_dotenv
-# from riotwatcher import LolWatcher, ApiError
-# import json
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+from riotwatcher import LolWatcher, ApiError
+import json
 
-# load_dotenv()
-# KEY = os.getenv('RIOT_KEY')
+load_dotenv()
+KEY = os.getenv('RIOT_KEY')
 
-# LolUsers = {}
+LolUsers = {}
 
-# class LeagueCommands(commands.Cog):
-#     '''
-#     A cog for getting information from the Riot games API using riotwatcher.
-#     '''
+class LeagueCommands(commands.Cog):
+    '''
+    A cog for getting information from the Riot games API using riotwatcher.
+    '''
 
-#     watcher = LolWatcher(KEY)
-#     region = 'na1'
+    watcher = LolWatcher(KEY)
+    region = 'na1'
 
-#     @commands.command()
-#     async def register_username(ctx, payload):
+    # @commands.command()
+    # async def register_username(ctx, payload):
+    #     username = ' '
+    #     LolUsers[ctx.author] = username
+
+def setup(bot):
+    bot.add_cog(LeagueCommands(bot))
--- cogs/tronald.py
+++
@@ -1,9 +0,0 @@
-# import discord
-# from discord.ext import commands
-# import requests
-
-# class Tronald(commands.Cogs):
-#     '''
-#     Implementation of Tronald API.
-#     '''
-
